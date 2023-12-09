c19bb5b485ef37811de50a7097865e2dcae05687
Eli Céspedes <eli.cespedes@protonmail.com>
2023-12-09 20:27:35 -0300

    primer cambio

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def def_matrix(n):
+    array_zero = np.zeros( (int(n), int(n)) )
+    return array_zero
+
+def rellenar_matriz(m):
+    
+
+#main():
+    #hacer matriz
+    #rellenar matrix
+    #metodo gauss-jordan
+    #dar soluciones
+    
+    
+
+#if __name__ == "__main__":
+#    main()
+
