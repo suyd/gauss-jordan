b4b5c9dc77bb81ff9248a810ebb610da63453260
Eli Céspedes <eli.cespedes@protonmail.com>
2023-12-11 06:06:43 -0300

    Efectivamente, sin clases, pero tambien, listo.

--- gui.py
+++ gui.py
@@ -1,70 +1,116 @@
 import PySimpleGUI as sg      
-import matriz as matrix
+import matriz as matrix_defs
+import numpy as np
+#variable global
+flag = [False]
 
-def change_value():
-    layout = [[sg.Text("Entrar valor", size=(15,1)), sg.InputText()],
-              [sg.Submit(), sg.Cancel()]]
+def modify_flag(flag):
+    flag[0] = True
+    
+def return_value(text):
+    layout = [[sg.Text(text, size=(15,1)), sg.InputText()],
+              [sg.Submit("Siguiente"), sg.Cancel("Cancelar")]]
 
     window = sg.Window("Reducción Gauss Jordan", layout)
     event, values = window.read()
 
     input = 0
 
-    if event == "Submit":
+    if values[0] == '':
+        modify_flag(flag)
+        return 0
+
+    if event == "Siguiente":
         input = values[0]
 
-    
+    if event == "Cancelar":
+        window.close
+
     window.close()
     return int(input)
 
-def window_modify_matrix(m):
-    layout = [[sg.Text('Matriz', font='Default 25')],
+def display_matrix(matrix, msg):
+    layout = [[sg.Text(msg, font='Default 25')],
              [sg.Text(size=(15,1), key='-MESSAGE-', font='Default 20')]]
 
-    layout += [[sg.Button(str(m.Matrix[row][col]), size=(4,2), pad=(1,1), border_width=2, key=(row,col)) for col in range(int(m.MAX_COL))] for row in range(int(m.MAX_ROWS))]
+    max_row, max_column = matrix.shape
+    #row, col are for the gui
+    layout += [[sg.Text(str(matrix[row][col]), size=(4,2), pad=(1,1), border_width=2, key=(row,col)) for col in range(int(max_column))] for row in range(int(max_row))]
 
-    layout += [[sg.Cancel()]]
-
-    window = sg.Window("Matriz", layout, modal=True)
+    layout += [[sg.Button("Siguiente")]]
+    
+    window = sg.Window("", layout)
 
     event, values = window.read()
 
-    if event == "Exit" or event == sg.WIN_CLOSED or event == "Cancel":
+    window.close() 
+
+def display_string(string):
+    layout = [[sg.Text(string)], [sg.Button("Continuar")]]
+    window = sg.Window("", layout)
+    event, values = window.read()
+    window.close()
+
+
+
+def window_modify_matrix(matrix, max_row, max_column, flag):
+    layout = [[sg.Text('Matriz | Soluciones', font='Default 25')],
+             [sg.Text(size=(15,1), key='-MESSAGE-', font='Default 20')]]
+
+    #row, col are for the gui
+    layout += [[sg.Button(str(matrix[row][col]), size=(4,2), pad=(1,1), border_width=2, key=(row,col)) for col in range(int(max_column))] for row in range(int(max_row))]
+
+    layout += [[sg.Button("Gauss")],[sg.Button("Inversa")],[sg.Cancel("Cerrar")]]
+
+    window = sg.Window("Reducción Gauss Jordan", layout)
+
+    event, values = window.read()
+
+    if event == "Inversa":
         window.close()
-        return -1
+        inverse_matrix = matrix_defs.calculate_inverse(matrix[:, :-1])
+        msg = "La matriz inversa es: "
+        display_matrix(inverse_matrix, msg)
 
-    else:
-        m.Matrix[event[0], event[1]] = change_value()
+
+    if event == "Gauss":
+        window.close()
+        #display_matrix(matrix, max_row, max_column)
+        reduction_steps, solutions, infinite_solutions, rango_A, rango_B = matrix_defs.gauss_jordan_reduction(matrix)
+        for step in reduction_steps:
+            display_string(step)
+        if(infinite_solutions):
+            msg_soluciones = "El sistema tiene soluciones infinitas"
+        else:
+            msg_soluciones = "El sistema tiene soluciones finitas"
+
+        display_string(msg_soluciones)
+        
+        msg_rango = "El rango de la matriz original es: " + str(rango_A) + "\nEl rango de la matriz ampliada es: " + str(rango_B) 
+        display_string(msg_rango)
+        modify_flag(flag)
+            
+
+    if event == "Exit" or event == sg.WIN_CLOSED or event == "Cerrar":
+        modify_flag(flag)
+        window.close()
+    print(event)
+    if isinstance(event, tuple):
+        msg = str(str(event[0]+1) + " , " + str(event[1]+1) + ":")
+        matrix[event[0], event[1]] = return_value(msg)
 
     window.close()
-    
-
-def window_get_size():
-    layout = [[sg.Text('Tamaño de Matriz Cuadrada')],
-             [sg.InputText()],
-             [sg.Submit(), sg.Cancel()]]
-
-    window = sg.Window('Reducción Gauss Jordan', layout)    
-
-    event, values = window.read()
-
-    if event == "Submit":
-        input = values[0]
-
-    
-    window.close()
-    return int(input)
-
+    return matrix
 
 def event_loop():
     matriz_A = np.array([])
     matriz_B = np.array([])
-    size = window_get_size()
-    matriz_A = matriz.gen_matrix(matriz_A, size, size)
-    matriz_B = matriz.gen_matrix(matriz_B, size, 1)
+    msg = "Ingrese la cantidad de variables:"
+    size = return_value(msg)
+    matriz_A = matrix_defs.gen_matrix(matriz_A, size, size)
+    matriz_B = matrix_defs.gen_matrix(matriz_B, size, 1)
+    matriz_ampliada = np.concatenate((matriz_A, matriz_B), axis=1)
 
-    while True:
-        m = window_modify_matrix(m)
-        if m == -1:
-            break
+    while flag[0] == False:
+        matriz_ampliada = window_modify_matrix(matriz_ampliada, size, size+1, flag)
     
--- matriz.py
+++ matriz.py
@@ -14,7 +14,41 @@ def fill_matrix_manual(matrix, rows, columns):
             matrix[j][i] = a
     return matrix
 
-    
+#def gauss_jordan_reduction(matrix, const_vector):
+def gauss_jordan_reduction(aug_matrix):
+    rows, cols = aug_matrix.shape
+    steps = []  # Almacenar los pasos de reducción
+
+    for i in range(rows):
+        # Normalizar la fila para tener un 1 en la diagonal
+        diagonal_element = aug_matrix[i, i]
+        aug_matrix[i] = aug_matrix[i] / diagonal_element
+        steps.append(f"Paso {i + 1}: Normalizar fila {i + 1} ->\n{aug_matrix}\n")
+
+        # Hacer ceros en las otras filas en la misma columna
+        for j in range(rows):
+            if i != j:
+                factor = aug_matrix[j, i]
+                aug_matrix[j] = aug_matrix[j] - factor * aug_matrix[i]
+                steps.append(f"Paso {i + 1}: Hacer ceros en fila {j + 1} ->\n{aug_matrix}\n")
+
+    # Obtener el vector de soluciones
+    solutions = aug_matrix[:, -1]
+    # Verificar si hay filas con ceros en la parte de coeficientes y un valor no cero en el lado derecho
+    infinite_solutions = any(np.all(aug_matrix[:, :-1] == 0, axis=1) & (aug_matrix[:, -1] != 0))
+
+    rango_A = np.linalg.matrix_rank(aug_matrix[:, :-1])
+    rango_B = np.linalg.matrix_rank(aug_matrix)
+    return steps, solutions, infinite_solutions, rango_A, rango_B
+
+def calculate_inverse(matrix):
+    # Calcular la matriz inversa si es cuadrada
+    inverse_matrix = None
+    if matrix.shape[0] == matrix.shape[1]:  # Verificar si es cuadrada
+        inverse_matrix = np.linalg.inv(matrix)
+
+    return inverse_matrix
+   
 def test():
     matrix_A = np.array([])
     matrix_B = np.array([])
@@ -24,9 +58,19 @@ def test():
     matrix_A = gen_matrix(matrix_A, size, size)
     matrix_B = gen_matrix(matrix_B, size, 1)
 
+    matrix_A = fill_matrix_manual(matrix_A, size, size)
     matrix_B = fill_matrix_manual(matrix_B, size, 1)
 
-    print (matrix_A,matrix_B)
+    aug_matrix = np.concatenate((matrix_A, matrix_B), axis=1)
+
     #ask user for the matrix, and the solutions
-    matrix_ampliada = np.concatenate((matrix_A, matrix_B), axis=1)
-    print(matrix_ampliada)
+    reduction_steps, solutions = gauss_jordan_reduction(aug_matrix)
+
+    print(reduction_steps)
+    for step in reduction_steps:
+        print(step)
+
+# Imprimir el vector de soluciones
+    print("Vector de soluciones:", solutions)
+    
+#test()
